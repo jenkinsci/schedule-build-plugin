55b3c63420f5f6fe5681f25d52e9968df37fb173
Julian Neuhaus <julian.neuhaus@brain-activit.com>
2017-02-17 16:50:47 +0100

    updated JenkinsApi
    replaced AbstractProject with Job

--- src/main/java/org/jenkinsci/plugins/schedulebuild/ScheduleBuildTransientProjectActionFactory.java
+++ src/main/java/org/jenkinsci/plugins/schedulebuild/ScheduleBuildTransientProjectActionFactory.java
@@ -2,16 +2,24 @@ package org.jenkinsci.plugins.schedulebuild;
 
 import hudson.Extension;
 import hudson.model.Action;
-import hudson.model.TransientProjectActionFactory;
-import hudson.model.AbstractProject;
+import hudson.model.Job;
+import jenkins.model.TransientActionFactory;
 
 import java.util.Collection;
 import java.util.Collections;
 
 @Extension
-public final class ScheduleBuildTransientProjectActionFactory extends TransientProjectActionFactory {
+public final class ScheduleBuildTransientProjectActionFactory extends TransientActionFactory<Job> {
+   
     @Override
-    public Collection<? extends Action> createFor(final AbstractProject target) {
+    public Class<Job> type()
+    {
+        return Job.class;
+    }
+
+    @Override
+    public Collection<? extends Action> createFor(Job target)
+    {
         return Collections.singleton(new ScheduleBuildAction(target));
     }
 }
