6af0b09182950d6541922ed1cb5b9d59915815ba
ArKniiD <n.hallouin@gmail.com>
2017-02-24 15:46:01 +0100

    Added timezone management for default scheduled time and launching time calculation.

--- src/main/java/org/jenkinsci/plugins/schedulebuild/ScheduleBuildAction.java
+++ src/main/java/org/jenkinsci/plugins/schedulebuild/ScheduleBuildAction.java
@@ -1,18 +1,12 @@
 package org.jenkinsci.plugins.schedulebuild;
 
-import hudson.StructuredForm;
-import hudson.model.Action;
-import hudson.model.AbstractProject;
-import hudson.model.Descriptor.FormException;
-import hudson.model.Hudson;
-import hudson.model.Job;
-import hudson.util.FormValidation;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.Date;
+
 import javax.servlet.ServletException;
-import net.sf.json.JSONObject;
+
 import org.kohsuke.stapler.HttpResponse;
 import org.kohsuke.stapler.HttpResponses;
 import org.kohsuke.stapler.QueryParameter;
@@ -20,91 +14,112 @@ import org.kohsuke.stapler.Stapler;
 import org.kohsuke.stapler.StaplerProxy;
 import org.kohsuke.stapler.StaplerRequest;
 
+import hudson.model.AbstractProject;
+import hudson.model.Action;
+import hudson.model.Hudson;
+import hudson.model.Job;
+import hudson.model.Descriptor.FormException;
+import hudson.util.FormValidation;
+import net.sf.json.JSONObject;
+
 public class ScheduleBuildAction implements Action, StaplerProxy {
-    private final static long securityMargin = 120*1000;
-    private final static long oneDay = 24*3600*1000;
-    
+    private final static long securityMargin = 120 * 1000;
+    private final static long oneDay = 24 * 3600 * 1000;
+
     private final AbstractProject target;
 
     public ScheduleBuildAction(final AbstractProject target) {
         this.target = target;
     }
 
-    public AbstractProject<?,?> getOwner() {
+    public AbstractProject<?, ?> getOwner() {
         return target;
     }
 
-    
+    @Override
     public String getIconFileName() {
         return target.hasPermission(Job.BUILD) && this.target.isBuildable() ? "/plugin/schedule-build/images/schedule.png" : null;
     }
 
+    @Override
     public String getDisplayName() {
         return target.hasPermission(Job.BUILD) && this.target.isBuildable() ? Messages.ScheduleBuildAction_DisplayName() : null;
     }
-    
+
+    @Override
     public String getUrlName() {
         return "schedule";
     }
-    
+
     public boolean schedule(StaplerRequest req, JSONObject formData) throws FormException {
-            return true;
+        return true;
     }
-    
+
+    @Override
     public Object getTarget() {
         target.checkPermission(Job.BUILD);
         return this;
     }
-    
+
     public String getIconPath() {
         return Hudson.getInstance().getRootUrl() + "plugin/schedule-build/";
     }
-           
+
     public String getDefaultDate() {
         Date buildtime = getDefaultDateObject();
         return dateFormat().format(buildtime);
     }
-    
+
     public Date getDefaultDateObject() {
-        Date buildtime = new Date(), now = new Date(),
-                defaultScheduleTime = new ScheduleBuildGlobalConfiguration().getDefaultScheduleTimeObject();
+        Date buildtime = new Date(), now = new Date(), defaultScheduleTime = new ScheduleBuildGlobalConfiguration().getDefaultScheduleTimeObject();
+        DateFormat dateFormat = dateFormat();
+        try {
+            now = dateFormat.parse(dateFormat.format(now));
+        } catch (ParseException e) {
+        }
         buildtime.setHours(defaultScheduleTime.getHours());
         buildtime.setMinutes(defaultScheduleTime.getMinutes());
         buildtime.setSeconds(defaultScheduleTime.getSeconds());
-        
-        if(now.getTime() > buildtime.getTime()) {
+
+        if (now.getTime() > buildtime.getTime()) {
             buildtime.setTime(buildtime.getTime() + ScheduleBuildAction.oneDay);
         }
-        
+
         return buildtime;
     }
-    
+
     public FormValidation doCheckDate(@QueryParameter String date) {
         Date ddate, now = new Date();
+        DateFormat dateFormat = dateFormat();
         try {
-             ddate = dateFormat().parse(date);
+            ddate = dateFormat.parse(date);
+            now = dateFormat.parse(dateFormat.format(now));
+        } catch (ParseException ex) {
+            return FormValidation.error(Messages.ScheduleBuildAction_ParsingError());
         }
-        catch(ParseException ex) {
-            return FormValidation.error(Messages.ScheduleBuildAction_ParsingError());   
-        }
-                      
-        if(now.getTime() > ddate.getTime() + ScheduleBuildAction.securityMargin) // 120 sec security margin
-            return FormValidation.error(Messages.ScheduleBuildAction_DateInPastError());   
-                
-        return FormValidation.ok();       
+
+        if (now.getTime() > ddate.getTime() + ScheduleBuildAction.securityMargin) // 120 sec security margin
+            return FormValidation.error(Messages.ScheduleBuildAction_DateInPastError());
+
+        return FormValidation.ok();
     }
+
     long quietperiod;
 
-    public long getQuietPeriodInSeconds(){
-        return quietperiod/1000;
+    public long getQuietPeriodInSeconds() {
+        return quietperiod / 1000;
     }
 
-
     public HttpResponse doNext(StaplerRequest req) throws FormException, ServletException, IOException {
-        //Deprecated function StructureForm.get()
-        //JSONObject param = StructuredForm.get(req);
+        // Deprecated function StructureForm.get()
+        // JSONObject param = StructuredForm.get(req);
         JSONObject param = req.getSubmittedForm();
         Date ddate = getDefaultDateObject(), now = new Date();
+        DateFormat dateFormat = dateFormat();
+        try {
+            now = dateFormat.parse(dateFormat.format(now));
+        } catch (ParseException e) {
+        }
 
         if (param.containsKey("date")) {
             try {
@@ -123,6 +138,8 @@ public class ScheduleBuildAction implements Action, StaplerProxy {
     }
 
     private DateFormat dateFormat() {
-        return DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, Stapler.getCurrentRequest().getLocale());
+        DateFormat df = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, Stapler.getCurrentRequest().getLocale());
+        df.setTimeZone(new ScheduleBuildGlobalConfiguration().getTimeZoneObject());
+        return df;
     }
 }
--- src/main/java/org/jenkinsci/plugins/schedulebuild/ScheduleBuildGlobalConfiguration.java
+++ src/main/java/org/jenkinsci/plugins/schedulebuild/ScheduleBuildGlobalConfiguration.java
@@ -1,47 +1,61 @@
 package org.jenkinsci.plugins.schedulebuild;
 
-import hudson.Extension;
-import hudson.util.FormValidation;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.Date;
-import jenkins.model.GlobalConfiguration;
-import jenkins.model.GlobalConfigurationCategory;
-import net.sf.json.JSONObject;
+import java.util.TimeZone;
+
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.Stapler;
 import org.kohsuke.stapler.StaplerRequest;
 
+import hudson.Extension;
+import hudson.model.Descriptor.FormException;
+import hudson.util.FormValidation;
+import jenkins.model.GlobalConfiguration;
+import jenkins.model.GlobalConfigurationCategory;
+import net.sf.json.JSONObject;
+
 @Extension
 public class ScheduleBuildGlobalConfiguration extends GlobalConfiguration {
     private Date defaultScheduleTime;
-    
+    private String timeZone;
+
     public ScheduleBuildGlobalConfiguration() {
-        this.defaultScheduleTime = new Date(0,0,0,22,0);
+        this.defaultScheduleTime = new Date(0, 0, 0, 22, 0);
+        this.timeZone = TimeZone.getDefault().getID();
         load();
     }
-   
+
     @Override
     public GlobalConfigurationCategory getCategory() {
         return new GlobalConfigurationCategory.Unclassified();
     }
-    
+
     public String getDefaultScheduleTime() {
         return getTimeFormat().format(this.defaultScheduleTime);
-    } 
+    }
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public TimeZone getTimeZoneObject() {
+        return TimeZone.getTimeZone(timeZone);
+    }
 
     private DateFormat getTimeFormat() {
         return DateFormat.getTimeInstance(DateFormat.SHORT, Stapler.getCurrentRequest().getLocale());
     }
-    
+
     public Date getDefaultScheduleTimeObject() {
         return this.defaultScheduleTime;
-    } 
-    
+    }
+
     public FormValidation doCheckDefaultScheduleTime(@QueryParameter String value) {
         try {
             getTimeFormat().parse(value);
-        } catch(ParseException ex) {
+        } catch (ParseException ex) {
             return FormValidation.error(Messages.ScheduleBuildGlobalConfiguration_ParsingError());
         }
         return FormValidation.ok();
@@ -49,12 +63,13 @@ public class ScheduleBuildGlobalConfiguration extends GlobalConfiguration {
 
     @Override
     public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
-        if(json.containsKey("defaultScheduleTime")) {
+        if (json.containsKey("defaultScheduleTime") && json.containsKey("timeZone")) {
             try {
                 this.defaultScheduleTime = getTimeFormat().parse(json.getString("defaultScheduleTime"));
+                this.timeZone = json.getString("timeZone");
                 save();
                 return true;
-            } catch(ParseException ex) {
+            } catch (ParseException ex) {
             }
         }
         return false;
