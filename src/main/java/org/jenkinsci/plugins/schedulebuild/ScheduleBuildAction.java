4652b3e1efdf2f2cee48e662dea296385500fef1
Martin Spielmann <mail@martinspielmann.de>
2017-06-14 15:53:57 +0200

    Fix csrf problem, moved javascript into resource file

--- src/main/java/org/jenkinsci/plugins/schedulebuild/ScheduleBuildAction.java
+++ src/main/java/org/jenkinsci/plugins/schedulebuild/ScheduleBuildAction.java
@@ -1,18 +1,12 @@
 package org.jenkinsci.plugins.schedulebuild;
 
-import hudson.model.Action;
-import hudson.model.AbstractProject;
-import hudson.model.Descriptor.FormException;
-import hudson.model.Hudson;
-import hudson.model.Job;
-import hudson.util.FormValidation;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.Date;
+
 import javax.servlet.ServletException;
-import jenkins.model.Jenkins;
-import net.sf.json.JSONObject;
+
 import org.kohsuke.stapler.HttpResponse;
 import org.kohsuke.stapler.HttpResponses;
 import org.kohsuke.stapler.QueryParameter;
@@ -20,98 +14,110 @@ import org.kohsuke.stapler.Stapler;
 import org.kohsuke.stapler.StaplerProxy;
 import org.kohsuke.stapler.StaplerRequest;
 
-public class ScheduleBuildAction implements Action, StaplerProxy {
-    private final static long securityMargin = 120*1000;
-    private final static long oneDay = 24*3600*1000;
-    
-    private final Job<?,?> target;
+import hudson.model.Action;
+import hudson.model.Job;
+import hudson.model.Descriptor.FormException;
+import hudson.util.FormValidation;
+import jenkins.model.Jenkins;
+import net.sf.json.JSONObject;
 
-    public ScheduleBuildAction(final Job<?,?> target) {
+public class ScheduleBuildAction implements Action, StaplerProxy {
+    private final static long securityMargin = 120 * 1000;
+    private final static long oneDay = 24 * 3600 * 1000;
+
+    private final Job<?, ?> target;
+
+    public ScheduleBuildAction(final Job<?, ?> target) {
         this.target = target;
     }
 
-    public Job<?,?> getOwner() {
+    public Job<?, ?> getOwner() {
         return target;
     }
 
-    
+
+    @Override
     public String getIconFileName() {
-        return target.hasPermission(Job.BUILD) && this.target.isBuildable() ? "/plugin/schedule-build/images/schedule.png" : null;
+        return target.hasPermission(Job.BUILD) && target.isBuildable() ? "/plugin/schedule-build/images/schedule.png" : null;
     }
 
+    @Override
     public String getDisplayName() {
-        return target.hasPermission(Job.BUILD) && this.target.isBuildable() ? Messages.ScheduleBuildAction_DisplayName() : null;
+        return target.hasPermission(Job.BUILD) && target.isBuildable() ? Messages.ScheduleBuildAction_DisplayName() : null;
     }
-    
+
+    @Override
     public String getUrlName() {
         return "schedule";
     }
-    
-    public boolean schedule(StaplerRequest req, JSONObject formData) throws FormException {
-            return true;
+
+    public boolean schedule(final StaplerRequest req, final JSONObject formData) throws FormException {
+        return true;
     }
-    
+
+    @Override
     public Object getTarget() {
         target.checkPermission(Job.BUILD);
         return this;
     }
-    
+
     public String getIconPath() {
-        
+
         Jenkins instance = Jenkins.getInstance();
-        
-        if(instance != null)
-        {
+
+        if (instance != null) {
             String rootUrl = instance.getRootUrl();
-            
-            if(rootUrl != null)
+
+            if (rootUrl != null) {
                 return rootUrl + "plugin/schedule-build/";
+            }
         }
-        
+
         throw new IllegalStateException("couldn't load rootUrl");
     }
-           
+
     public String getDefaultDate() {
         Date buildtime = getDefaultDateObject();
         return dateFormat().format(buildtime);
     }
-    
+
     public Date getDefaultDateObject() {
         Date buildtime = new Date(), now = new Date(),
                 defaultScheduleTime = new ScheduleBuildGlobalConfiguration().getDefaultScheduleTimeObject();
         buildtime.setHours(defaultScheduleTime.getHours());
         buildtime.setMinutes(defaultScheduleTime.getMinutes());
         buildtime.setSeconds(defaultScheduleTime.getSeconds());
-        
-        if(now.getTime() > buildtime.getTime()) {
+
+        if (now.getTime() > buildtime.getTime()) {
             buildtime.setTime(buildtime.getTime() + ScheduleBuildAction.oneDay);
         }
-        
+
         return buildtime;
     }
-    
-    public FormValidation doCheckDate(@QueryParameter String date) {
+
+    public FormValidation doCheckDate(@QueryParameter final String date) {
         Date ddate, now = new Date();
         try {
-             ddate = dateFormat().parse(date);
+            ddate = dateFormat().parse(date);
+        } catch (ParseException ex) {
+            return FormValidation.error(Messages.ScheduleBuildAction_ParsingError());
         }
-        catch(ParseException ex) {
-            return FormValidation.error(Messages.ScheduleBuildAction_ParsingError());   
+
+        if (now.getTime() > ddate.getTime() + ScheduleBuildAction.securityMargin) {
+            return FormValidation.error(Messages.ScheduleBuildAction_DateInPastError());
         }
-                      
-        if(now.getTime() > ddate.getTime() + ScheduleBuildAction.securityMargin) // 120 sec security margin
-            return FormValidation.error(Messages.ScheduleBuildAction_DateInPastError());   
-                
-        return FormValidation.ok();       
+
+        return FormValidation.ok();
     }
+
     long quietperiod;
 
-    public long getQuietPeriodInSeconds(){
-        return quietperiod/1000;
+    public long getQuietPeriodInSeconds() {
+        return quietperiod / 1000;
     }
 
 
-    public HttpResponse doNext(StaplerRequest req) throws FormException, ServletException, IOException {
+    public HttpResponse doNext(final StaplerRequest req) throws FormException, ServletException, IOException {
         //Deprecated function StructureForm.get()
         //JSONObject param = StructuredForm.get(req);
         JSONObject param = req.getSubmittedForm();
