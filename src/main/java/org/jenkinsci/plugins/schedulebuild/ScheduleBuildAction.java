52742ae9a91eeb76c84842ea8f3b2e2d8ea4cd81
Mark Waite <mark.earl.waite@gmail.com>
2021-10-23 10:51:15 -0600

    Check item permission before data sanity checks
    https://www.jenkins.io/doc/developer/security/form-validation/#checking-permissions
    provides details of the check.
    
    This is cleanup, not a security risk, because the checks do not modify
    state and do not need elevated permissions for the operations they
    perform.

--- src/main/java/org/jenkinsci/plugins/schedulebuild/ScheduleBuildAction.java
+++ src/main/java/org/jenkinsci/plugins/schedulebuild/ScheduleBuildAction.java
@@ -9,6 +9,7 @@ import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.HttpResponse;
 import org.kohsuke.stapler.HttpResponses;
 import org.kohsuke.stapler.QueryParameter;
@@ -18,6 +19,7 @@ import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.interceptor.RequirePOST;
 
 import hudson.model.Action;
+import hudson.model.Item;
 import hudson.model.Job;
 import hudson.model.ParametersDefinitionProperty;
 import hudson.model.Descriptor.FormException;
@@ -104,7 +106,11 @@ public class ScheduleBuildAction implements Action, StaplerProxy {
     }
 
     @RequirePOST
-    public FormValidation doCheckDate(@QueryParameter String date) {
+    public FormValidation doCheckDate(@QueryParameter String date, @AncestorInPath Item item) {
+        if (item == null) {
+            return FormValidation.ok();
+        }
+        item.checkPermission(Item.CONFIGURE);
         Date ddate, now = new Date();
         DateFormat dateFormat = dateFormat();
         try {
@@ -126,7 +132,11 @@ public class ScheduleBuildAction implements Action, StaplerProxy {
     }
 
     @RequirePOST
-    public HttpResponse doNext(StaplerRequest req) throws FormException, ServletException, IOException {
+    public HttpResponse doNext(StaplerRequest req, @AncestorInPath Item item) throws FormException, ServletException, IOException {
+        if (item == null) {
+            return FormValidation.ok();
+        }
+        item.checkPermission(Item.CONFIGURE);
         // Deprecated function StructureForm.get()
         // JSONObject param = StructuredForm.get(req);
         JSONObject param = req.getSubmittedForm();
